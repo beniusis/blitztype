65d7c10fb9b75eaba1a32d08db1b5864b9194080
beniusis <rokasbeniu@gmail.com>
2024-04-09 18:47:36 +0300

    feat: created 2 custom animations - drop and fade

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,29 @@ tailwind.config = {
         primary: '#96b8d3',
         secondary: '#2b5c85',
         accent: '#559cd6'
+      },
+      keyframes: {
+        drop: {
+          from: {
+            transform: 'translateY(-400%) scale(0.5)'
+          },
+          to: {
+            scale: 1,
+            transform: 'translateY(0%) scale(1)'
+          }
+        },
+        fade: {
+          from: {
+            opacity: 0
+          },
+          to: {
+            opacity: 1
+          }
+        }
+      },
+      animation: {
+        fade: 'fade 1s ease-in',
+        drop: 'drop 1s ease-in'
       }
     }
   }
