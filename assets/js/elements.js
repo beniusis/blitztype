917bcadae8d2f4ec0fb6c2e9fe1733d6fe420449
beniusis <rokasbeniu@gmail.com>
2024-04-11 11:51:11 +0300

    feat: created the file for storing and exporting used HTML elements

---
+++ assets/js/elements.js
@@ -0,0 +1,6 @@
+export const statsBtn = document.querySelector('#stats');
+export const resetBtn = document.querySelector('#reset');
+export const restartBtn = document.querySelector('#restart');
+export const timer = document.querySelector('#timer');
+export const caret = document.querySelector('#caret');
+export const textField = document.querySelector('#textfield');
