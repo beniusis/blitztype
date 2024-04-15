fb751ce35b2bbf8a41c04dd6c953d974ff927b63
beniusis <rokasbeniu@gmail.com>
2024-04-15 16:04:23 +0300

    feat: function to populate the text field with the words

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -4,7 +4,7 @@
  * @function getWords
  * @returns Promise<Array>
  */
-export const getWords = async () => {
+const getWords = async () => {
   const response = await fetch('../assets/data/words.json');
   const data = await response.json();
   return randomizeWords(data.list);
@@ -22,4 +22,23 @@ const randomizeWords = (words) => {
     [words[i], words[j]] = [words[j], words[i]];
   }
   return words;
+};
+
+/**
+ * Populates the text field with words.
+ * @async
+ * @function populate
+ * @param {HTMLElement} field
+ */
+export const populate = async (field) => {
+  const words = await getWords();
+
+  words.forEach((word) => {
+    let letters = '';
+    word.split('').forEach((letter) => {
+      letters += `<span class='letter'>${letter}</span>`;
+    });
+    letters += `<span class='letter'>&nbsp;</span>`;
+    field.innerHTML += `<span class='word'>${letters}</span>`;
+  });
 };
