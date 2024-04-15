df959f2d48e94516d39977bed33ce449c98f565a
beniusis <rokasbeniu@gmail.com>
2024-04-15 11:15:12 +0300

    refactor: switched to arrow functions syntax, fixed annotation structure

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -4,22 +4,22 @@
  * @function getWords
  * @returns Promise<Array>
  */
-export async function getWords() {
+export const getWords = async () => {
   const response = await fetch('../assets/data/words.json');
   const data = await response.json();
   return randomizeWords(data.list);
-}
+};
 
 /**
  * Randomizes the order of words in the array.
  * @function randomizeWords
- * @param words Array
+ * @param {Array} words
  * @returns Array
  */
-function randomizeWords(words) {
+const randomizeWords = (words) => {
   for (let i = 0; i < words.length; i++) {
     const j = Math.floor(Math.random() * (i + 1));
     [words[i], words[j]] = [words[j], words[i]];
   }
   return words;
-}
+};
