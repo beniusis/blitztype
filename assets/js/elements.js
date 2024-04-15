4b980caf84351900471bb70bae81ec541d58256d
beniusis <rokasbeniu@gmail.com>
2024-04-15 16:09:08 +0300

    style: variable name change, reduced the size of words list

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -1,6 +1,6 @@
 export const statsBtn = document.querySelector('#stats');
 export const resetBtn = document.querySelector('#reset');
 export const restartBtn = document.querySelector('#restart');
-export const timerElement = document.querySelector('#timer');
-export const caretElement = document.querySelector('#caret');
-export const textField = document.querySelector('#textfield');
+export const timerDiv = document.querySelector('#timer');
+export const caretDiv = document.querySelector('#caret');
+export const textDiv = document.querySelector('#textfield');
