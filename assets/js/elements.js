9c42def710596dd400ba2c7ed70b7243132d1342
beniusis <rokasbeniu@gmail.com>
2024-04-18 14:24:14 +0300

    chore: updated elements.js accordingly to the changed ids

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -1,7 +1,17 @@
-export const statsBtn = document.querySelector('#stats');
+export const viewStatisticsBtn = document.querySelector('#view-statistics');
 export const resetBtn = document.querySelector('#reset');
 export const restartBtn = document.querySelector('#restart');
-export const timerDiv = document.querySelector('#timer');
-export const caretDiv = document.querySelector('#caret');
-export const textDiv = document.querySelector('#textfield');
 export const loader = document.querySelector('#loader');
+export const timer = document.querySelector('#timer');
+export const caret = document.querySelector('#caret');
+export const textPrompt = document.querySelector('#text-prompt');
+export const resultModal = document.querySelector('#result-modal');
+export const resultModalCloseBtn = document.querySelector(
+  '#close-result-modal'
+);
+export const comparison = document.querySelector('#comparison');
+export const wpmResult = document.querySelector('#wpm-result');
+export const accuracyResult = document.querySelector('#accuracy-result');
+export const statsModal = document.querySelector('#stats-modal');
+export const statsModalCloseBtn = document.querySelector('#close-stats-modal');
+export const statsTable = document.querySelector('#stats-table');
