d6988c06e9ba60670203418d1e1c6c736631d947
beniusis <rokasbeniu@gmail.com>
2024-04-18 17:49:05 +0300

    chore: constants.js for storing constant values

---
+++ assets/js/constants.js
@@ -0,0 +1,7 @@
+export const STATS_COLUMNS = [{ name: 'ID', sort: false }, 'WPM', 'Accuracy'];
+export const STATS_PAGINATION_CONFIG = {
+  limit: 10,
+  summary: false,
+  prevButton: false,
+  nextButton: false
+};
